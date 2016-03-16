508167bb673ee9711ac42d7c30d5c18b2d52e776
MeTaNoV <pascal.gula@gmail.com>
2016-03-16 18:20:31 +0100

    basic routing set up!

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,6 +8,7 @@
   
   // Sets app default base URL
   app.baseUrl = '/';
+
   if (window.location.port === '') {  // if production
     // Uncomment app.baseURL below and
     // set app.baseURL to '/your-pathname/' if running from folder in production
