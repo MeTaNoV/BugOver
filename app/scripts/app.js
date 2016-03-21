1e7ef07f5b3363af2a73910bb27eb851a9e84dc4
MeTaNoV <pascal.gula@gmail.com>
2016-03-21 09:14:09 +0100

    working integration for games page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,25 @@
+var bgoApp = {};
+
 (function(document) {
   'use strict';
 
   // Grab a reference to our auto-binding template
   // and give it some initial binding values
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
-  var app = document.querySelector('#app');
-  
-  // Sets app default base URL
-  app.baseUrl = '/';
+  bgoApp = document.querySelector('#bgoApp');
+
+  // Sets bgoApp default base URL
+  bgoApp.baseUrl = '/';
 
   if (window.location.port === '') {  // if production
-    // Uncomment app.baseURL below and
-    // set app.baseURL to '/your-pathname/' if running from folder in production
-    // app.baseUrl = '/polymer-starter-kit/';
+    // Uncomment bgoApp.baseURL below and
+    // set bgoApp.baseURL to '/your-pathname/' if running from folder in production
+    // bgoApp.baseUrl = '/polymer-starter-kit/';
   }
 
-  app.firebaseUrl = 'https://bugover.firebaseio.com';
+  bgoApp.firebaseUrl = 'https://bugover.firebaseio.com';
 
-  app.displayInstalledToast = function() {
+  bgoApp.displayInstalledToast = function() {
     // Check to make sure caching is actually enabled—it won't be in the dev environment.
     if (!Polymer.dom(document).querySelector('platinum-sw-cache').disabled) {
       Polymer.dom(document).querySelector('#caching-complete').show();
@@ -26,13 +28,14 @@
 
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
-  app.addEventListener('dom-change', function() {
-    console.log('Our app is ready to rock!');
+  bgoApp.addEventListener('dom-change', function() {
+    console.log('Our bgoApp is ready to rock!');
   });
-  
+
   // See https://github.com/Polymer/polymer/issues/1381
   window.addEventListener('WebComponentsReady', function() {
     // imports are loaded and elements have been registered
+    console.log('WebComponentsReady');
   });
 
 })(document);
